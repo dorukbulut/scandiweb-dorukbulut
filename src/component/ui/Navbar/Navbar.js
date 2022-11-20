1cdde3ab18d874e29e21f01d548784811590736a
Doruk Bulut <dorukbulutinfo@gmail.com>
2022-11-20 19:28:23 +0300

    Start currency dropdown

--- src/component/ui/Navbar/Navbar.js
+++ src/component/ui/Navbar/Navbar.js
@@ -2,16 +2,27 @@ import React from "react";
 import "./Navbar.css";
 import ShopLogo from "../../../assets/navlogo.svg";
 import CartLogo from "../../../assets/cartlogo.svg";
+import Arrow from "../../../assets/arrow.svg";
 
 class Navbar extends React.Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+
+        }
+    }
 
     render() {
         return(
             <nav  id="navbar">
                 <div className="cat-items">
-                    <div>WOMEN</div>
-                    <div>MEN</div>
-                    <div>KIDS</div>
+                    <div className="">
+                        <div className="item selected">WOMEN</div>
+                        <hr></hr>
+                    </div>
+                    <div className="item">MEN</div>
+                    <div className="item">KIDS</div>
                 </div>
 
                 <div className="nav-logo">
@@ -19,8 +30,19 @@ class Navbar extends React.Component {
                 </div>
 
                 <div className="nav-action">
-                    <div>
+                    <div className="currency">
+                        <div className="">
 
+                        </div>
+
+                        <div className="">
+                            <img src={Arrow}/>
+                            <div className="currency-options">
+                                <div className="curr-option">$ USD</div>
+                                <div className="curr-option">€ EUR</div>
+                                <div className="curr-option">¥ JPY</div>
+                            </div>
+                        </div>
                     </div>
 
                     <div>
